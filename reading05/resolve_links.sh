de27e4c8a1eb7184905300b7f62ba3db3c6d427f
tfrances <tfrances@nd.edu>
2016-02-15 10:39:04 -0500

    reading 05

--- reading05/resolve_links.sh
+++ reading05/resolve_links.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #resolve system links
+
 if [ -d $1 ];
-    then for f in *; do
+    then for f in $1/*; do
         if [ -L $f ];
             then echo "$f links to $(readlink $f) "
         fi
     done    
-fi 
+fi
