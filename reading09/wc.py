718975922ffa5690d0e64ab76cef2a5ad270f910
tfrances <tfrances@nd.edu>
2016-03-21 23:58:15 -0400

    reading 09

--- reading09/wc.py
+++ reading09/wc.py
@@ -1,11 +1,11 @@
 import sys
 import getopt
 
-COUNT = False
+DISP_COUNT = False
 NEWLINE = False
-WORD = False
+PRINT_WORDS = False
 
-count = 0
+word_count = 0
 new_line = 0
 words = 0
 
@@ -17,11 +17,11 @@ except getopt.GetoptError as e:
 
 for o, a in opts:
 	if o == "-c":
-		COUNT = True
+		DISP_COUNT = True
 	elif o == "-l":
 		NEWLINE = True
 	elif o == "-w":
-		WORD = True
+		PRINT_WORDS = True
 	else:
 		print "ERROR: enter valid flag"
 		sys.exit(1)
@@ -36,17 +36,17 @@ for path in args:
 		stream = open(path)
 
 	for line in stream:
-		count = count + len(line)
+		word_count = word_count + len(line)
 		if "\n" in line:
 			new_line = new_line + 1
-		if WORD:
+		if PRINT_WORDS:
 			words = words + len(line.split())
 
-	if COUNT:
-		print count
+	if DISP_COUNT:
+		print word_count
 	if NEWLINE:
 		print new_line
-	if WORD:
+	if PRINT_WORDS:
 		print words
 
 	stream.close()
