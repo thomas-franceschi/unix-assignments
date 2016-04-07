004a21dbbcb036b8cc2d56ca184e2abdc82d9392
tfrances <tfrances@nd.edu>
2016-04-07 00:06:12 -0400

    homework 08

---
+++ homework08/rorschach.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python2.7
+
+import getopt
+import os
+import sys
+import re
+import yaml
+import fnmatch
+import shlex
+import time
+import errno
+import string
+
+# Global Variables
+NAME = os.path.basename(sys.argv[0])
+YAML = '.rorschach.yml'
+DIRECTORIES = '.'
+SECONDS = 2
+
+VERBOSE = False
+
+# Functions
+def error(message, exit_code=1):
+    print >>sys.stderr, message
+    sys.exit(exit_code)
+
+def usage(exit_code=0):
+    error('''Usage: {} [-r YAML -t SECONDS] DIRECTORIES...
+
+Options:
+
+    -r YAML    Path to rules file (default is .rorschach.yml)
+    -t SECONDS  Time between scans (default is 2 seconds)
+    -v          Display verbose debugging output
+    -h          Show this help message'''
+    .format(NAME), exit_code)
+
+
+
+def check_file(path, data):
+    '''This function checks each file to see if it matches any of the rules and then executes the action.'''
+    if VERBOSE:
+        print "Checking for pattern matches..."
+    for x in data:
+        if fnmatch.fnmatch(path, x['pattern']):
+            execute_action(path, x)
+            
+def check_directory(data, dictionary):
+    '''This function walks the specified directory and checks each file if it matches any of the rules.'''
+    for root, dirs, files in os.walk(DIRECTORIES):
+        files = [f for f in files if not f[0] == '.']
+        for name in files:
+            fname = os.path.join(root, name)
+            if fname in dictionary:
+                if os.path.exists(fname):
+                    prev = dictionary[fname]
+                    current = os.path.getmtime(fname)
+                    if prev < current:
+                        dictionary[fname] = current
+                        check_file(fname, data)
+            else:
+                diction[fname] = os.path.getmtime(fname)
+                check_file(fname, data)
+
+def execute_action(path, data):
+    '''This function executes the action.'''
+    if VERBOSE:
+        print "Forking process..."
+    command = data['action'].format(path=path)
+    arguments = shlex.split(command)
+    try:
+        pid = os.fork()
+        if pid == 0:
+            try:
+                if VERBOSE:
+                    print "Executing command..."
+                os.execvp(arguments[0], arguments)
+            except OSError as e:
+                error('Unable to exec: {}', e)
+        else:
+            try:
+                pid, status = os.wait()
+            except OSError as e:
+                if e.errno == errno.EINTR:
+                    pid, status = os.wait()
+                else:
+                    error(e)
+    except OSError as e:
+        error('Unable to fork: {}', e)
+
+# Parse Command line arguments
+try:
+    options, arguments = getopt.getopt(sys.argv[1:], "hr:t:v")
+except getopt.GetoptError as e:
+    error(e)
+
+for option, value in options:
+    if option == '-h':
+        usage(0)
+    if option == '-r':
+        YAML = value
+    if option == '-t':
+        SECONDS = value
+    if option == '-v':
+        VERBOSE = True
+
+# Main Execution
+direct = sys.argv[len(sys.argv) - 1]
+if(os.path.isdir(direct)):
+    DIRECTORIES = direct
+
+info = open(YAML)
+data = yaml.load(info)
+info.close()
+
+diction = {}
+if VERBOSE:
+    print "Determining files already in directory..."
+for root, dirs, files in os.walk(DIRECTORIES):
+    files = [f for f in files if not f[0] == '.']
+    for name in files:
+        fname = os.path.join(root, name)
+        diction[fname] = os.path.getmtime(fname)
+
+if VERBOSE:
+    print "Checking for new or modified files..."
+
+try:
+    while True:
+        check_directory(data, diction)
+except KeyboardInterrupt:
+    os.system('clear')
+    sys.exit(0)
